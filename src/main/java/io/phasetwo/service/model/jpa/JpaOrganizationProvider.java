deec8ff6eb174d5f290b1a62510607ec2e7c4167
Patrick Hamann <patrick@fastly.com>
2023-10-31 11:10:22 +0000

    Remove lowercasing logic from the searchForOrgs attirbutes predicate query

--- src/main/java/io/phasetwo/service/model/jpa/JpaOrganizationProvider.java
+++ src/main/java/io/phasetwo/service/model/jpa/JpaOrganizationProvider.java
@@ -249,8 +249,8 @@ public class JpaOrganizationProvider implements OrganizationProvider {
 
           attributePredicates.add(
               builder.and(
-                  builder.equal(builder.lower(attributesJoin.get("name")), key.toLowerCase()),
-                  builder.equal(builder.lower(attributesJoin.get("value")), value.toLowerCase())));
+                  builder.equal(attributesJoin.get("name"), key),
+                  builder.equal(attributesJoin.get("value"), value)));
           break;
       }
     }
